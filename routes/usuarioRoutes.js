4bb2748942ebca57788be78e4c65087b9dc2e18c
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-09-23 15:33:41 -0300

    refactor(routes/usuarioRoutes): Atualiza rotas para usar o modelo Mongoose Usuario

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Usuario = require('../models/Usuario');
+const Usuario = require('../models/Usuario'); // Importe o modelo do Mongoose
 const { geraToken, verificaToken } = require('../auth');
 
 // Rota para login e obtenção de token JWT
@@ -23,57 +23,100 @@ router.post('/login', async (req, res) => {
 });
 
 // Rota para criar um usuário
-router.post('/usuarios', (req, res) => {
+router.post('/usuarios', async (req, res) => {
   const { nome, email, senha, nickname, idade, cidade } = req.body;
-  const novoUsuario = new Usuario(nome, email, senha, nickname, idade, cidade);
-  cache.put(email, novoUsuario);
-  res.json(novoUsuario);
+
+  try {
+    const novoUsuario = new Usuario({
+      nome,
+      email,
+      senha,
+      nickname,
+      idade,
+      cidade,
+      ativo: true, // Se desejar, você pode definir isso como ativo por padrão
+    });
+
+    await novoUsuario.save();
+    res.json(novoUsuario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensagem: 'Erro ao criar o usuário' });
+  }
 });
 
 // Rota para listar todos os usuários
-router.get('/usuarios', (req, res) => {
-  const usuarios = cache.keys().map((key) => cache.get(key));
-  res.json(usuarios);
+router.get('/usuarios', async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.json(usuarios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensagem: 'Erro ao listar os usuários' });
+  }
 });
 
 // Rota para buscar um usuário por email
-router.get('/usuarios/:email', (req, res) => {
+router.get('/usuarios/:email', async (req, res) => {
   const email = req.params.email;
-  const usuario = cache.get(email);
-  if (usuario) {
+
+  try {
+    const usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+    }
+
     res.json(usuario);
-  } else {
-    res.status(404).json({ mensagem: 'Usuário não encontrado' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensagem: 'Erro ao buscar o usuário' });
   }
 });
 
 // Rota para atualizar um usuário por email
-router.put('/usuarios/:email', (req, res) => {
+router.put('/usuarios/:email', async (req, res) => {
   const email = req.params.email;
-  const usuario = cache.get(email);
-  if (usuario) {
+
+  try {
+    const usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+    }
+
     const { nome, senha, nickname, idade, cidade } = req.body;
+
     usuario.nome = nome;
     usuario.senha = senha;
     usuario.nickname = nickname;
     usuario.idade = idade;
     usuario.cidade = cidade;
-    cache.put(email, usuario);
+
+    await usuario.save();
     res.json(usuario);
-  } else {
-    res.status(404).json({ mensagem: 'Usuário não encontrado' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensagem: 'Erro ao atualizar o usuário' });
   }
 });
 
 // Rota para excluir um usuário por email
-router.delete('/usuarios/:email', (req, res) => {
+router.delete('/usuarios/:email', async (req, res) => {
   const email = req.params.email;
-  const usuario = cache.get(email);
-  if (usuario) {
-    cache.del(email);
+
+  try {
+    const usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+    }
+
+    await usuario.remove();
     res.json({ mensagem: 'Usuário excluído com sucesso' });
-  } else {
-    res.status(404).json({ mensagem: 'Usuário não encontrado' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensagem: 'Erro ao excluir o usuário' });
   }
 });
 
