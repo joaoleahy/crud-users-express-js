6ec2a9141868d1378861da269aedcb5aab47cae6
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-09-23 13:05:23 -0300

    chore(app): Atualiza para configuração do MongoDB e rotas de usuário

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+const db = require('./config/db'); // Importa a configuração do MongoDB
+const usuarioRoutes = require('./routes/usuarioRoutes'); // Importa as rotas de usuários
 
 app.use(express.json());
+
+// Conecta-se ao MongoDB usando a configuração em db.js
+db.once('open', () => {
+  console.log('Conexão com o MongoDB estabelecida com sucesso.');
+});
+
+// Usa as rotas de usuário definidas em usuarioRoutes.js
+app.use('/usuarios', usuarioRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
